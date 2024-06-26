9c481e69164adbd4bd52345299fcd950b56c2140
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-06-26 19:50:34 +0100

    refactor: update metadata in layout and home page
    Update the metadata in the layout and home page components to reflect the changes made to the title, description, and keywords. This ensures that the correct information is displayed in the browser tab and search engine results.

--- app/my-route/nested-route/page.tsx
+++ app/my-route/nested-route/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen justify-center items-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Home
+        Nested Route
       </h1>
     </main>
   );
