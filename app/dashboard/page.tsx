c9d92fd724d726a0bc6a534cb83c562c5a3b2fc7
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-06-26 20:02:37 +0100

    refactor: remove nested route and my route components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen justify-center items-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        My Route
+        Dashboard
       </h1>
     </main>
   );
--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen justify-center items-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Nested Route
+        Settings
       </h1>
     </main>
   );
