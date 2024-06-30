36c6a2593f31196945fbac4b28ea8caa5207c27b
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-06-30 10:05:06 +0100

    Add solution

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex flex-col h-screen w-screen`}>
-        {children}
+        <div className="h-full flex flex-col p-6 text-center">
+          <p className="mb-6 leading-7 [&:not(:first-child)]:mt-6">
+            Root Layout
+          </p>
+          <div className="flex-1 border rounded-lg h-full">{children}</div>
+        </div>
       </body>
     </html>
   );
---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex h-full justify-center items-center">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Profile
+      </h1>
+    </main>
+  );
+}
