90da29a351169eddcc2bdacd5f407702b763206c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-01 13:45:26 +0100

    Deploy

--- app/show/[id]/loading.tsx
+++ app/show/[id]/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex justify-center items-center">
       Loading...
     </div>
   );
