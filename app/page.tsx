c2547d9f69899eb334c75eeeefc8038c1bf3f42e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-01 13:50:33 +0100

    chore: Remove prefetch attribute from Link component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <Link className="underline" href="/show/1" prefetch={false}>
+      <Link className="underline" href="/show/1">
         Link
       </Link>
     </div>
