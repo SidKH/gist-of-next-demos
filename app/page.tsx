ad1e8256730868ed74fcfeb9953fe11c66dbb52b
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-01 13:28:25 +0100

    refactor: update home page heading to use Next.js branding

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center">
+    <main className="flex flex-1 justify-center h-full items-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Next.js
       </h1>
