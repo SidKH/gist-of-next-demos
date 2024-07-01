04693228c7c84e46f9e3567155eec804905ad668
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-01 21:59:30 +0100

    Add text to show prefetching on viewport

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen justify-center items-center">
-      <Link className="underline" prefetch={true} href="/show/1">
+    <div className="flex flex-col min-h-screen justify-center items-center py-8">
+      {Array.from(Array(80).keys()).map((i) => {
+        return (
+          <p className="text-muted-foreground" key={i}>
+            Text
+          </p>
+        );
+      })}
+      <Link className="underline text-blue-600" prefetch={true} href="/show/1">
         Link
       </Link>
     </div>
