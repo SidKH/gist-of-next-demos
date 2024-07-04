049f715abde29d68644b20f105d34d147dcc13bd
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-04 22:44:50 +0100

    Add redirect demo

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,76 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().min(1).email(),
+  password: z.string().min(4),
+});
+
 export default function Home() {
+  async function signup(formData: FormData) {
+    "use server";
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const validation = formSchema.safeParse({ email, password });
+      if (!validation.success) {
+        return { errors: "Validation error" };
+      }
+      redirect("/dashboard");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen justify-center items-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next.js
-      </h1>
-    </main>
+    <form
+      action={signup}
+      className="w-full h-screen flex justify-center items-center"
+    >
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign up</CardTitle>
+          <CardDescription>Enter your email below to sign up.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              value="alex@gmail.com"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              value="1231231313"
+              name="password"
+              type="password"
+              required
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Sign up</Button>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
