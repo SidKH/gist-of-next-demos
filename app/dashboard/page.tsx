7e055013580b377d4c8e2c28b7a203cc1d58ef7e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-10 00:41:22 +0100

    Finished demo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,11 @@
+import { fakeFetch } from "@/lib/utils";
+
 export default async function Page() {
+  await fakeFetch({
+    name: "Page",
+    time: 1500,
+  });
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <h1 className="text-5xl font-bold">Dashboard</h1>
