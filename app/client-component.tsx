9da27483c4498f7c563ed1e77a589d6968f64ee8
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-19 12:14:28 +0100

    Update demo

--- app/client-component.tsx
+++ app/client-component.tsx
@@ -3,16 +3,18 @@
 import { redirect, useRouter } from "next/navigation";
 
 export function ClinetComponent() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <h2 className="text-4xl font-bold tracking-tight">Client component</h2>
       <button
-        onClick={() => {
+        onMouseOver={() => {
           redirect("/dashboard");
         }}
         className="rounded-full bg-white px-3.5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
-        Redirect
+        Redirect to Dashboard
       </button>
     </div>
   );
