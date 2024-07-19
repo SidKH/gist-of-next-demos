17ba97645feca04c1f98bbc5dc87b63f836ec574
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-19 12:22:59 +0100

    chore: Add keyboard shortcut to redirect to dashboard

--- app/client-component.tsx
+++ app/client-component.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export function ClinetComponent() {
   const router = useRouter();
 
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "r" || event.key === "R") {
+        router.push("/dashboard");
+      }
+    };
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [router]);
+
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <h2 className="text-4xl font-bold tracking-tight">Client component</h2>
-      <button
-        onMouseOver={() => {
-          redirect("/dashboard");
-        }}
-        className="rounded-full bg-white px-3.5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-      >
-        Redirect to Dashboard
-      </button>
+      <p className="text-sm text-stone-700">
+        Press {`"R"`} to redirect to dashboard
+      </p>
     </div>
   );
 }
