66bf6fa07fb2aa4fdc9732cb9439214c4d2be6a1
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-19 12:13:14 +0100

    Add demo

---
+++ app/client-component.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { redirect, useRouter } from "next/navigation";
+
+export function ClinetComponent() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      <h2 className="text-4xl font-bold tracking-tight">Client component</h2>
+      <button
+        onClick={() => {
+          redirect("/dashboard");
+        }}
+        className="rounded-full bg-white px-3.5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        Redirect
+      </button>
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 w-screen h-screen">
+      <h2 className="text-4xl font-bold tracking-tight">Dashboard</h2>
+    </div>
+  );
+}
