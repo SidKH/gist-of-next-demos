dd73f47778377f736c0ab9836d963c73db352f25
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-20 20:00:53 +0100

    fix: Update client-component to use startTransition for redirecting to dashboard

--- app/client-component.tsx
+++ app/client-component.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { redirect, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { startTransition, useEffect } from "react";
 
 export function ClinetComponent() {
   const router = useRouter();
@@ -9,7 +9,7 @@ export function ClinetComponent() {
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.key === "r" || event.key === "R") {
-        router.push("/dashboard");
+        startTransition(() => redirect("/dashboard"));
       }
     };
     window.addEventListener("keydown", handleKeyPress);
