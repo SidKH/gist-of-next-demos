2d6d49afd86e08e95606330a3565989758e325ed
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-22 12:57:15 +0100

    Update demo

--- app/show/[id]/opengraph-image.tsx
+++ app/show/[id]/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const alt = "About Acme";
+export const alt = "Acme";
 export const size = {
   width: 1200,
   height: 630,
