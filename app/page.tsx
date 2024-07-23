bc03eb161f7a8fabf9485f61d3348a58ce8fbbc5
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 16:58:34 +0100

    Add blur

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen justify-center items-center flex-col gap-4">
       <p>IP Address:</p>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl blur-md">
         {headers().get("x-forwarded-for")}
       </h1>
     </main>
