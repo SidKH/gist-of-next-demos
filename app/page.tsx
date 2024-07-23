a1b8d57803b63f212d03bd49c2b3e3271db41639
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 17:07:55 +0100

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { headers } from "next/headers";
-
 export default function Home() {
-  const ip = headers().get("x-forwarded-for");
-
   return (
     <main className="flex min-h-screen justify-center items-center flex-col gap-4">
       <p>IP Address:</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        {ip}
+        ?
       </h1>
     </main>
   );
