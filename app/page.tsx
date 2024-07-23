feba1a70ad127e8db1994b8dbf3574f0d8f56bbc
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 17:23:51 +0100

    Finalize demo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex mt-24 justify-center items-center flex-col gap-4">
       <p>IP Address:</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        198.51.100.23
+        {ip}
       </h1>
     </main>
   );
