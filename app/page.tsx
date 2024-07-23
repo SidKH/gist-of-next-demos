940acc00bd16aa454f1e068aead553183aae89a1
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 16:53:58 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { headers } from "next/headers";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center">
+    <main className="flex min-h-screen justify-center items-center flex-col gap-4">
+      <p>IP Address:</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next.js
+        {headers().get("x-forwarded-for")}
       </h1>
     </main>
   );
