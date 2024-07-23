13875f30e272c11b8bd78ee5a51e001aa5252b4f
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 17:09:31 +0100

    Fix height

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center flex-col gap-4">
+    <main className="flex mt-24 justify-center items-center flex-col gap-4">
       <p>IP Address:</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         ?
