27146cd4a6892eed64337c074e33d42dbd99807f
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-23 17:13:48 +0100

    fake id

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { headers } from "next/headers";
+
 export default function Home() {
+  const ip = headers().get("x-forwarded-for");
+
   return (
     <main className="flex mt-24 justify-center items-center flex-col gap-4">
       <p>IP Address:</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        ?
+        198.51.100.23
       </h1>
     </main>
   );
