3f4a9f63a32981b9ddbcb3a11ee60592e0337e11
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-07-29 13:06:05 +0100

    Update demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,49 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { Suspense, useState, useTransition } from "react";
+import { useState, useTransition } from "react";
+
+export default function Page() {
+  const [tab, setTab] = useState(1);
+  return (
+    <>
+      <div className="p-4 max-w-sm mx-auto flex justify-center gap-5 border-b">
+        <TabButton onClick={() => setTab(1)} isActive={tab === 1}>
+          Tab 1
+        </TabButton>
+        <TabButton onClick={() => setTab(2)} isActive={tab === 2}>
+          Tab 2
+        </TabButton>
+        <TabButton onClick={() => setTab(3)} isActive={tab === 3}>
+          Tab 3
+        </TabButton>
+      </div>
+      {tab === 1 && <Tab1 />}
+      {tab === 2 && <Tab2 />}
+      {tab === 3 && <Tab3 />}
+    </>
+  );
+}
 
 function Tab1() {
   return <p className="text-3xl font-bold mt-40 text-center">Tab 1</p>;
 }
 
+function SlowComponent() {
+  let startTime = performance.now();
+  while (performance.now() - startTime < 10) {}
+  return <li className="item">Slow component (10ms)</li>;
+}
+
 function Tab2() {
   let items = [];
   for (let i = 0; i < 100; i++) {
-    items.push(<SlowComponent key={i} index={i} />);
+    items.push(<SlowComponent key={i} />);
   }
   return (
     <ul className="text-center max-w-sm mx-auto overflow-auto h-[50vh]">
       {items}
     </ul>
   );
-}
-
-function SlowComponent({ index }: { index: number }) {
-  let startTime = performance.now();
-  while (performance.now() - startTime < 10) {}
-
-  return <li className="item">Slow component (10ms)</li>;
 }
 
 function Tab3() {
@@ -50,27 +71,5 @@ function TabButton({
     >
       {children}
     </button>
-  );
-}
-
-export default function Page() {
-  const [tab, setTab] = useState(1);
-  return (
-    <>
-      <div className="p-4 max-w-sm mx-auto flex justify-center gap-5 border-b">
-        <TabButton isActive={tab === 1} onClick={() => setTab(1)}>
-          Tab 1
-        </TabButton>
-        <TabButton isActive={tab === 2} onClick={() => setTab(2)}>
-          Tab 2
-        </TabButton>
-        <TabButton isActive={tab === 3} onClick={() => setTab(3)}>
-          Tab 3
-        </TabButton>
-      </div>
-      {tab === 1 && <Tab1 />}
-      {tab === 2 && <Tab2 />}
-      {tab === 3 && <Tab3 />}
-    </>
   );
 }
