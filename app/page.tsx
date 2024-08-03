45ebb3a4be890301be78613e8ba48a41a817d573
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-08-03 16:27:58 +0100

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+
+import { useSyncExternalStore } from "react";
+
+function subscribe(callback: () => void) {
+  window.addEventListener("online", callback);
+  window.addEventListener("offline", callback);
+  return () => {
+    window.removeEventListener("online", callback);
+    window.removeEventListener("offline", callback);
+  };
+}
+
+function getSnapshot() {
+  return navigator.onLine;
+}
+
 export default function Home() {
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
   return (
     <main className="flex min-h-screen justify-center items-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next.js
+      <h1 className="text-5xl tracking-tight font-extrabold">
+        {isOnline ? (
+          <span className="flex gap-2">
+            👨‍💻
+            <span>Online</span>
+          </span>
+        ) : (
+          <span className="flex gap-2">
+            🏄‍♂️
+            <span>Offline</span>
+          </span>
+        )}
       </h1>
     </main>
   );
