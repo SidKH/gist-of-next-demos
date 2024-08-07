02f6f96d7c738ed9f71afc936b71c10aba324c48
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-08-07 21:01:33 +0100

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+import fs from "fs";
+import path from "path";
+
 export default function Home() {
+  async function action() {
+    "use server";
+    const filePath = path.join(process.cwd(), "number.txt");
+    let number = parseInt(fs.readFileSync(filePath, "utf8"));
+    console.log(`Action ${number}`);
+    await sleep(1000);
+    fs.writeFileSync(filePath, (number + 1).toString(), "utf8");
+  }
+
   return (
     <main className="flex min-h-screen justify-center items-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next.js
-      </h1>
+      <form action={action}>
+        <button className="px-4 py-2 bg-stone-950 active:ring ring-offset-2 ring-stone-950 rounded-md text-white text-sm">
+          Server action
+        </button>
+      </form>
     </main>
   );
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
