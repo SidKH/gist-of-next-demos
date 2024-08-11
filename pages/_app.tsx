450cc60cf918e97f17f8875749a5d6efb73d8703
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-08-11 14:42:24 +0100

    Remove inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from "next/app";
 import "./global.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
+    <div>
       <Component {...pageProps} />
     </div>
   );
