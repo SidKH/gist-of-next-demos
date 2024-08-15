c8bbf014bb4dda2a29fff6644bc9f691f3ccf69e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-08-15 14:31:42 +0100

    Move supabase server

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/supabase/server";
 
 export default async function Home() {
   const supabase = createClient();
