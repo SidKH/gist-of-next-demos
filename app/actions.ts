d50c10b0f9cadb73ccc9b5b7cce9528057dc18e0
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-08-27 14:33:39 +0100

    Uncommit action

--- app/actions.ts
+++ app/actions.ts
@@ -3,5 +3,5 @@
 import { redirect } from "next/navigation";
 
 export async function myAction(formData: FormData) {
-  //redirect(`/confetti?message=${formData.get("message")}`);
+  redirect(`/confetti?message=${formData.get("message")}`);
 }
