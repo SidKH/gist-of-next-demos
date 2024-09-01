8d903246949cf80c66edd3f6f69126266760efd6
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-09-01 16:33:13 +0100

    Update demo

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,11 @@
 "use server";
 
-import { sleep } from "@/lib/utils";
-
 export async function fetchServerAction() {
   console.log("Fetching data (1s)");
   await sleep(1000);
   return Math.random();
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,8 +1,8 @@
-import { sleep } from "@/lib/utils";
-import { NextResponse } from "next/server";
-
 export async function GET() {
-  console.log("Fetching data (1s)");
   await sleep(1000);
-  return NextResponse.json(Math.random());
+  return Response.json(Math.random());
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
