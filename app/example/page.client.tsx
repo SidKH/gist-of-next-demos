65eb3a72af30c52f1541d58a4d25c63ad00ea228
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-09-06 17:55:41 +0100

    Demo

---
+++ app/example/page.client.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Mesh } from "three";
+
+export default function Page() {
+  return (
+    <div className="h-screen">
+      <Canvas className="h-screen">
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
+}
+
+function Box(props: { position: [number, number, number] }) {
+  const meshRef = useRef<Mesh>(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta;
+    }
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
---
+++ app/example/page.tsx
@@ -0,0 +1,7 @@
+import PageClient from "./page.client";
+
+export const metadata = { title: "3D Cubes" };
+
+export default function Page() {
+  return <PageClient />;
+}
