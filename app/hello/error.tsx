ac196692ab319217a9db630163bd185d39c38e3c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-10-02 18:57:53 +0100

    demo

---
+++ app/hello/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
+export default function Error({ reset }: { reset: () => void }) {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <h2 className="text-lg">Something went wrong!</h2>
+      <Image src="/image.png" alt="Error" width={300} height={300} />
+      <Button
+        disabled={isPending}
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+        Try again
+      </Button>
+    </div>
+  );
+}
---
+++ app/hello/page.tsx
@@ -0,0 +1,17 @@
+export default async function Page() {
+  await sleep(300);
+
+  if (Math.random() > 0.5) {
+    throw new Error("Error thrown in component");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
