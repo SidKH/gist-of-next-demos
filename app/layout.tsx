fe704e928fa50efbb8f527b033842c8e81bc7166
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-10-11 22:35:19 +0100

    Return metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { headers } from "next/headers";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const headersList = headers();
-  const host = headersList.get("host") || "default-domain.com";
-
-  // Extract the domain (remove port if present)
-  const domain = host.split(":")[0];
-
-  console.log("Domain:", domain);
-
-  return {
-    title: "My App",
-    description: "My App Description",
-    metadataBase: new URL(`https://${domain}`),
-    alternates: {
-      canonical: "./",
-    },
-  };
-}
+export const metadata: Metadata = {
+  title: "My App",
+  description: "My App Description",
+  metadataBase: new URL("https://my-app.com"),
+  alternates: {
+    canonical: "./",
+  },
+};
 
 export default function RootLayout({
   children,
