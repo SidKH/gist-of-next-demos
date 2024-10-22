d667bf2bdd6d8b41d9020ff87a20b0524e062aa1
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-10-22 17:13:23 +0100

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default async function Home() {
   ).then((res) => res.json());
 
   return (
-    <main className="flex min-h-screen justify-center items-center">
-      <img src={dogImage} alt="dog" />
+    <main className="flex min-h-screen flex-col items-center p-8 gap-4">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Next 15
+      </h1>
+      <img className="max-w-xs" src={dogImage} alt="dog" />
     </main>
   );
 }
