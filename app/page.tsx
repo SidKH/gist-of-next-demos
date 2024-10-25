982850c9b12cbe2c46859eb9749765531c6c7352
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-10-25 12:32:56 +0100

    Fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-8 gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Next 15
       </h1>
