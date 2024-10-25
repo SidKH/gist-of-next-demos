bc39c335e3c57d1e649b8b82e929cdac59baa5d9
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-10-25 12:14:42 +0100

    15

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 export default async function Home() {
-  const { message: dogImage } = await fetch(
-    "https://dog.ceo/api/breeds/image/random"
-  ).then((res) => res.json());
-
   return (
     <main className="flex min-h-screen flex-col items-center p-8 gap-4">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Next 15
       </h1>
-      <img className="max-w-xs" src={dogImage} alt="dog" />
     </main>
   );
 }
