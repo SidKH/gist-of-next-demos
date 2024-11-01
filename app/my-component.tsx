0e0ad369c099ac022fe2b825e24e16c0170fa221
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-11-01 22:11:57 +0000

    Demo

---
+++ app/my-component.tsx
@@ -0,0 +1,7 @@
+export function MyComponent() {
+  return (
+    <h1 className="text-4xl font-bold tracking-tight">
+      My important SEO keyword
+    </h1>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import { MyComponent } from "./my-component";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next 15
-      </h1>
+      <MyComponent />
     </main>
   );
 }
