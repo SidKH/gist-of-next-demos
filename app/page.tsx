f6c8b6a2b2006e98a90d390ff89842f811efce4e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-11-18 14:43:46 +0000

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { Suspense } from "react";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Next 15
-      </h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RandomDog />
+      </Suspense>
     </main>
   );
+}
+
+async function RandomDog() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return <img className="max-w-sm" src={data.message} alt="Random dog" />;
 }
