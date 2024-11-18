a641b550bee4605a97c91b27b3e774364054f839
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-11-18 14:54:43 +0000

    Remove PPR

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    ppr: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
