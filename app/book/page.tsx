313c8bc86d807404ce03aefabdcb53d25aba4671
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-11-18 18:38:55 +0000

    Add suspense

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { fetchBook, fetchRating } from "@/lib/data";
-import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function Page() {
@@ -9,17 +9,20 @@ export default async function Page() {
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="max-w-xl flex w-full gap-6">
         <Image
-          className="flex-shrink-0"
           src={book.image}
-          alt={book.name}
+          className="flex-shrink-0"
           width={190.5}
           height={300}
+          alt={book.name}
         />
         <div>
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-2">
             {book.name}
           </h1>
           <p className="mb-2">{book.description}</p>
+          <Suspense fallback={<Skeleton className="mt-3 w-32 h-4" />}>
+            <Rating />
+          </Suspense>
         </div>
       </div>
     </main>
@@ -27,7 +30,7 @@ export default async function Page() {
 }
 
 async function Rating() {
-  const rating = await fetchRating();
+  const rating = await fetchRating(); // uses cookies
   return (
     <div className="flex items-center gap-1 items-center">
       <span className="text-sm font-bold mr-2">{rating}</span>
