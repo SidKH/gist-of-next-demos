e8cd9bf35e7dc23d0f4ba4d0014238e96d02b718
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-03 14:47:48 +0000

    Update template

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { MyComponent } from "./my-component";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <MyComponent />
       <Background />
+      Home
     </main>
   );
 }
