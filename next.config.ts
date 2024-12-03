c39462cb46f0576254f8786e0b9fed74a5536cde
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-03 15:40:08 +0000

    Remove copmiler

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
