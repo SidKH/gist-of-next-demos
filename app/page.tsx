5815710e3bd47a965e55cdf93e7acec857515d35
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-15 14:15:22 +0000

    update demo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   async function action() {
     "use server";
     const id = await primaryTask();
-    backgroundTask(id);
+    await backgroundTask(id);
     redirect("/success");
   }
 
