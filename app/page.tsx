73e9272fa37ec2bbd900aa8fa05bbf417ba6c682
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-15 14:09:02 +0000

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { waitUntil } from "@vercel/functions";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  async function action() {
+    "use server";
+    const id = await primaryTask();
+    waitUntil(backgroundTask(id));
+    redirect("/success");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Home
+      <form action={action}>
+        <Button type="submit">Server action</Button>
+      </form>
     </main>
   );
+}
+
+async function primaryTask() {
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  console.log("Primary task - 300ms");
+  return "string";
+}
+
+async function backgroundTask(param: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Background task - 1000ms");
 }
---
+++ app/success/page.tsx
@@ -0,0 +1,7 @@
+export default function Success() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h2 className="text-2xl font-bold">Success 🎉</h2>
+    </main>
+  );
+}
