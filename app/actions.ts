85a52fd23f1a51577e1fefe16579e6e45f70178c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-15 16:22:18 +0000

    Demo

--- app/actions.ts
+++ app/actions.ts
@@ -1,20 +1,19 @@
-import { waitUntil } from "@vercel/functions";
 import { redirect } from "next/navigation";
 
 export async function action() {
   "use server";
-  const id = await primaryTask();
-  waitUntil(backgroundTask(id));
+  backgroundTask();
+  await primaryTask();
   redirect("/success");
 }
 
 async function primaryTask() {
-  await new Promise((resolve) => setTimeout(resolve, 300));
-  console.log("Primary task - 300ms");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Primary task - 1000ms");
   return "string";
 }
 
-async function backgroundTask(param: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("Background task - 1000ms");
+async function backgroundTask() {
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  console.log("Background task - 300ms");
 }
