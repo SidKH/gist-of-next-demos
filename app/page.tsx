62aad7f0d8c4d1fe3a11d6f5f039c356a10e19de
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-16 14:08:56 +0000

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
-import { Button } from "@/components/ui/button";
-import { action } from "./actions";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const randomDog = await fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => data.message);
+
+  console.log("Render");
+  logPageViewTo3rdParty();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <form action={action}>
-        <Button type="submit">Server action</Button>
-      </form>
+      <h1 className="absolute top-5 w-full text-center">Random dog image</h1>
+      <div className="w-48 h-48 relative">
+        <Image
+          src={randomDog}
+          className="max-w-xs max-h-[240px] object-contain"
+          fill={true}
+          alt="Dog"
+        />
+      </div>
     </main>
   );
+}
+
+async function logPageViewTo3rdParty() {
+  // Simulate latency
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Logged page view to 3rd party");
 }
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.dog.ceo",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
