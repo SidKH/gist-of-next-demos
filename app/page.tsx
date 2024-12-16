de695323cccdf62b4607aed91929e849dd6c5cfc
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-16 14:59:57 +0000

    demo-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { after } from "next/server";
 
 export const dynamic = "force-dynamic";
 
@@ -7,11 +6,6 @@ export default async function Home() {
   const randomDog = await fetch("https://dog.ceo/api/breeds/image/random")
     .then((res) => res.json())
     .then((data) => data.message);
-
-  after(async () => {
-    // Execute this after component is rendered and sent to the user
-    await logPageViewTo3rdParty();
-  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
@@ -26,12 +20,4 @@ export default async function Home() {
       </div>
     </main>
   );
-}
-
-async function logPageViewTo3rdParty() {
-  // Simulate latency
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  // Test request with webhook.site
-  await fetch("https://webhook.site/47ed014e-4d2c-4b2c-95aa-d64791bcba83");
 }
