b3e8e8cee32b766c5983cf313e5c75a844efa07c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-17 19:26:25 +0000

    Demo

---
+++ app/[number]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      Loading...
+    </div>
+  );
+}
---
+++ app/[number]/page.tsx
@@ -0,0 +1,15 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ number: string }>;
+}) {
+  // Simulate latency
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const number = (await params).number;
+
+  return (
+    <div className="text-4xl font-bold tracking-tight w-screen h-screen flex items-center justify-center">
+      Page {number}
+    </div>
+  );
+}
