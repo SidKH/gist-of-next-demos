1716da8c9095bbfbe3ddcc03af7f2d1a52881ab6
Alex Sidorenko <my.mail.kharkov@gmail.com>
2024-12-27 15:33:06 +0000

    Remove delay

--- auth.js
+++ auth.js
@@ -1,16 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const {
-  handlers,
-  signIn,
-  signOut,
-  auth: originalAuth,
-} = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
 });
-
-export const auth = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 second delay
-  return originalAuth();
-};
