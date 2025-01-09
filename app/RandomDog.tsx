89ff2aa06a36441b1352c5f7b55f68f18ba72e86
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-09 18:10:19 +0000

    Update deps

---
+++ app/RandomDog.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import useSWR from "swr";
+import { getRandomDog } from "./data";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+
+export function RandomDog() {
+  const { data, isLoading, mutate, isValidating } = useSWR(
+    "randomDog",
+    getRandomDog
+  );
+
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <div className="w-64 h-64 flex justify-center items-center bg-zinc-50 p-8">
+        {isLoading || isValidating ? (
+          <Loader2 className="w-8 h-8 animate-spin" />
+        ) : (
+          <img className="max-w-full max-h-full" src={data} alt="Random Dog" />
+        )}
+      </div>
+      <Button onClick={() => mutate("randomDog")}>Another Dog</Button>
+    </div>
+  );
+}
