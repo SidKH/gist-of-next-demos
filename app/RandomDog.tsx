b8a89e9bc3bf542e0a9af00f5bef4cacd7b9486b
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-13 20:22:33 +0000

    Update UI

--- app/RandomDog.tsx
+++ app/RandomDog.tsx
@@ -14,13 +14,16 @@ export function RandomDog() {
   return (
     <div className="flex flex-col gap-4 items-center">
       <div className="w-64 h-64 flex justify-center items-center bg-zinc-50 p-8">
-        {isLoading || isRefetching ? (
+        {isLoading ? (
           <Loader2 className="w-8 h-8 animate-spin" />
         ) : (
           <img className="max-w-full max-h-full" src={data} alt="Random Dog" />
         )}
       </div>
-      <Button onClick={() => refetch()}>Another Dog</Button>
+      <Button disabled={isRefetching} onClick={() => refetch()}>
+        {isRefetching && <Loader2 className="w-4 h-4 animate-spin" />}
+        Another Dog
+      </Button>
     </div>
   );
 }
