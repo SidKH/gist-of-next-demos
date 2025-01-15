49ca8a34ff2bff2f82e52a45b62c10fcd79e27f8
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-15 19:25:36 +0000

    Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+import { revalidateTag } from "next/cache";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const dog = await fetch("https://dog.ceo/api/breeds/image/random", {
+    next: {
+      tags: ["randomDog"],
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data.message);
+
+  async function refetchDog() {
+    "use server";
+    revalidateTag("randomDog");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      Home
-    </main>
+    <div className="flex flex-col w-screen h-screen justify-center gap-4 items-center">
+      <div className="w-64 h-64 flex justify-center items-center bg-zinc-50 p-8">
+        <img className="max-w-full max-h-full" src={dog} alt="Random Dog" />
+      </div>
+      <form action={refetchDog}>
+        <Button>Another dog</Button>
+      </form>
+    </div>
   );
 }
