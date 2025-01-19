2aec978fb50645da3e6d8e620322dca532db4331
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-19 16:40:07 +0000

    Demo

---
+++ app/global-error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function GlobalError() {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+      </body>
+    </html>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 export default function Home() {
+  throw new Error("Test error");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       Home
