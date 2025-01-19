a70306bc1eebb692f4bd117175c71837eaf8d803
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-19 16:57:24 +0000

    Update demo

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,10 +1,20 @@
 "use client";
+import Link from "next/link";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 
 export default function GlobalError() {
   return (
     <html>
-      <body>
-        <h2>Something went wrong!</h2>
+      <body className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Ooops!</h1>
+        <p>Something went wrong!</p>
+        <Button className="mt-4" asChild>
+          <Link className="text-blue-500" href="/">
+            <ArrowLeft /> Back to home
+          </Link>
+        </Button>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Home
+      <h1 className="text-2xl font-bold">Home</h1>
     </main>
   );
 }
