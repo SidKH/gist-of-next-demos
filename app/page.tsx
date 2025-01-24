dc9bd130feb109d5533095f61016b9d2c4fb4ca9
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-01-24 21:37:20 +0000

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Counter } from "./counter";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <title>Server title</title>
+      <Counter />
     </main>
   );
 }
