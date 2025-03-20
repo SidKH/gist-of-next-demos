2d010e4b96bbdd81d53a41e49ea4336ba4a22586
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-03-20 15:38:51 +0000

    suspense-vs-loading

---
+++ app/my-page/page.tsx
@@ -0,0 +1,12 @@
+export default async function MyPage() {
+  return <MyComponent />;
+}
+
+async function MyComponent() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return (
+    <div className="flex h-screen w-screen items-center justify-center text-2xl font-medium">
+      MyPage
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Home
+      <Button asChild>
+        <Link href="/my-page">Go to My Page</Link>
+      </Button>
     </main>
   );
 }
