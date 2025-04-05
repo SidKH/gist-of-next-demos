0762c93bbafc63fe91b97b59aabafbdfa6038a4c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-05 15:14:21 +0100

    Update demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-import TheAnswer from "./the-answer";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center h-screen justify-center text-4xl font-bold">
-      <TheAnswer />
+      Hello world
     </div>
   );
 }
--- app/the-answer.tsx
+++
@@ -1,7 +0,0 @@
-export default function TheAnswer() {
-  // Secret business logic that should
-  // not be exposed to the client
-  const theAnswer = 10 * 5 - 4 * 2;
-
-  return <p>{theAnswer}</p>;
-}
