73cef173f9ba7617328b44852e1d7a99bb4b6414
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-07 15:42:17 +0100

    Update demo

--- app/template.tsx
+++ app/template.tsx
@@ -1,9 +1,5 @@
 import { unstable_ViewTransition as ViewTransition } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <ViewTransition name="page">{children}</ViewTransition>
-    </div>
-  );
+  return <ViewTransition name="page">{children}</ViewTransition>;
 }
