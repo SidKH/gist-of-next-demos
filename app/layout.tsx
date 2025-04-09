ba4acb72917dd98a8b338124ade0bb30a18c9aa5
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-09 14:49:37 +0100

    Refactor configuration and remove unused components and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,15 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <nav className="flex gap-4 p-4 text-sm justify-center">
-            <Link href="/page-1">Page 1</Link>
-            <Link href="/page-2">Page 2</Link>
-          </nav>
-        </header>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
