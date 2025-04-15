d0cefa7f2f3260647a95a7b5d28d430f09c7c1ee
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-15 14:25:08 +0100

    demo

--- app/client-component.tsx
+++ app/client-component.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from "react";
+"use client";
 
 export function ClientComponent() {
-  useEffect(() => {
-    throw new Error("ClientComponent");
-  }, []);
+  throw new Error("ClientComponent");
 
   return (
     <div onClick={() => alert("clicked")}>
