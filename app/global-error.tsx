e3636f20e720e68bb4bb3283c20d9ae174e5dfa1
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-15 14:50:22 +0100

    Remove GlobalError component

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Image from "next/image";
+import "./globals.css";
 
 export default function GlobalError() {
   return (
     <html>
-      <body className="flex justify-center items-center h-screen bg-[#FCFAFC]">
+      <body className="flex justify-center items-center h-screen w-screen bg-[#FCFAFC]">
         <Image
           src="/error.gif"
           alt="logo"
