7772e9e96b403e0290fa70a3cfa0ceb107c62bd0
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-18 15:03:53 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 export default function Home() {
+  console.log(
+    "Secret log from the server: password123"
+  );
+
   return (
     <div className="flex flex-col items-center h-screen justify-center text-4xl font-bold">
       Hello world
