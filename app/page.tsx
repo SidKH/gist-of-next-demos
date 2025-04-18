e1d2aa1fcf9d7b9928cedcf7b21e62389f92e8f2
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-04-18 19:05:16 +0100

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { redis } from "@/lib/redis";
+
+export default async function Home() {
+  const entry = (await redis.get("entry")) as string;
+
   return (
     <div className="flex flex-col items-center h-screen justify-center text-4xl font-bold">
-      Hello world
+      {entry}
     </div>
   );
 }
