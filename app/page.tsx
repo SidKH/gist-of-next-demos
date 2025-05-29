f233e55f5b06abb6d07cf047308c9c5ac20803ae
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-05-29 18:10:31 +0100

    Demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
-      Home
+      <Image
+        src="/image.png"
+        alt="logo"
+        width={536}
+        height={354}
+      />
     </div>
   );
 }
