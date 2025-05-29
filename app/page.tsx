ad593dcac970fd8ef7920287f4843ce9b81c3a41
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-05-29 18:55:39 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
       <Image
-        src="/image.png"
+        src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"
         alt="logo"
         width={536}
         height={354}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    remotePatterns: [
+      { hostname: "fastly.picsum.photos" },
+    ],
     minimumCacheTTL: 2678400, // 31 days
   },
 };
