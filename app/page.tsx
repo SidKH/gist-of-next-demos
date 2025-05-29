b78b3077690845c8eb2cfdfbd5dfcb05cc292dde
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-05-29 19:03:42 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
       <Image
-        src="https://picsum.photos/id/870/200/300"
+        src="https://picsum.photos/id/871/200/300"
         alt="logo"
         width={536}
         height={354}
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "picsum.photos" }],
-    minimumCacheTTL: 86400, // 1 day
   },
 };
 
