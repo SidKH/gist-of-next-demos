07b8ae33c86b3f05ea3189b5357bc5ace12a9ff4
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-05-29 18:08:23 +0100

    Update deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-async function Mycomponent() {
-  const data = await fetch(
-    "https://jsonplaceholder.typicode.com/posts/1"
-  ).then((res) => res.json());
-  return (
-    <div className="w-48 h-48 border rounded-md p-8">
-      {data.title}
-    </div>
-  );
-}
-
 export default function Home() {
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
-      <Mycomponent />
-      <Mycomponent />
+      Home
     </div>
   );
 }
