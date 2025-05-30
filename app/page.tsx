5d062cf5324d32c9c0f8b433d8d52af88bbe0011
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-05-30 15:34:00 +0100

    Requests

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+async function getDataA(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data A");
+    }, 1000);
+  });
+}
+
+async function getDataB(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data B");
+    }, 1000);
+  });
+}
+
+async function getDataC(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data C");
+    }, 1000);
+  });
+}
+
+export default async function Page() {
+  const dataA = await getDataA();
+  const dataB = await getDataB();
+  const dataC = await getDataC();
+
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
-      Home
+      <div>{dataA}</div>
+      <div>{dataB}</div>
+      <div>{dataC}</div>
     </div>
   );
 }
