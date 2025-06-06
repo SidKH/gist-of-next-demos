52800d342e8d3aee15f3137ad00acb257dc02b88
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-06-06 16:26:08 +0100

    res cloudinary

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className="flex gap-4 items-center h-screen justify-center">
-      <video src="/video.mp4" autoPlay loop muted />
+      <video
+        src="https://res.cloudinary.com/dulgvdcw5/video/upload/v1749223359/video_w97x6t.mp4"
+        autoPlay
+        loop
+        muted
+      />
     </div>
   );
 }
