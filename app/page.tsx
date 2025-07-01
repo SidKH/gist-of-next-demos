b693e2be1b31a0735c8e448204d65ba8c5b78578
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-01 16:23:35 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+// #region imports
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { revalidatePath } from "next/cache";
+import { createClient } from "redis";
+// #endregion
+
+const redis = await createClient({
+  url: process.env.REDIS_URL,
+}).connect();
+
+export default async function Home() {
+  const value = await redis.get("myKey");
+
   return (
-    <div className="flex gap-4 items-center h-screen justify-center">
-      Home
+    <div className="flex flex-col gap-8 items-center h-screen justify-center">
+      <div className="text-center text-4xl font-bold">
+        {value ?? "No value"}
+      </div>
+      <form
+        className="flex gap-4"
+        action={async (formData) => {
+          "use server";
+          const value = formData.get("value") as string;
+          await redis.set("myKey", value);
+          revalidatePath("/");
+        }}
+      >
+        <Input type="text" name="value" />
+        <Button type="submit" className="min-w-24">
+          Set
+        </Button>
+      </form>
     </div>
   );
 }
