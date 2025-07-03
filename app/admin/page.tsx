96a4afc043ad5ece02ee4c87730841286854f9b4
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-03 18:27:32 +0100

    demo

---
+++ app/admin/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+//import { isAdmin } from "@/lib/auht";
+//import { notFound } from "next/navigation";
+
+export default async function AdminPage() {
+  //const userIsAdmin = await isAdmin();
+  //if (!userIsAdmin) {
+  //  notFound();
+  //}
+
+  return (
+    <Button
+      onClick={async () => {
+        "use server";
+        console.log("Deleting important stuff 🚨");
+      }}
+    >
+      Delete important stuff
+    </Button>
+  );
+}
