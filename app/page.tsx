cf33178c0d64c519d3806fcf75c87b7edab4c78e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-11 22:42:12 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link";
+import { HoverPrefetchLink } from "@/components/hover-prefetch-link";
 
 export default function Home() {
   return (
     <ul className="flex flex-col p-24 divide-y">
       {[...Array(100)].map((_, index) => (
         <li key={index} className="py-6">
-          <Link
+          <HoverPrefetchLink
             className="hover:underline text-sm"
             href={`/my-route/${index + 1}`}
           >
             Route {index + 1}
-          </Link>
+          </HoverPrefetchLink>
         </li>
       ))}
     </ul>
--- components/hover-prefetch-link.tsx
+++ components/hover-prefetch-link.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Link, { LinkProps } from "next/link";
+import Link from "next/link";
 import { useState } from "react";
 
-export function HowerPrefetchLink(
-  props: LinkProps & React.ComponentProps<"a">
-) {
+export function HoverPrefetchLink(props) {
   const [active, setActive] = useState(false);
 
   return (
