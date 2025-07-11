1e83397cc7511f19c7c47c2e46d62fa4c0c3fde0
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-11 22:18:24 +0100

    timeout

--- app/my-route/[id]/page.tsx
+++ app/my-route/[id]/page.tsx
@@ -4,6 +4,9 @@ export default async function MyRoutePage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  await new Promise((resolve) =>
+    setTimeout(resolve, 1000)
+  );
   return (
     <div className="flex justify-center items-center h-screen text-3xl font-medium">
       My Route {id}
