8a2f2c8aa378a7ecb381849f83eca73b023a7cd1
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-11 22:01:54 +0100

    Return Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { HowerPrefetchLink } from "@/components/app-link";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <ul className="flex flex-col p-24 divide-y">
       {[...Array(100)].map((_, index) => (
         <li key={index} className="py-6">
-          <HowerPrefetchLink
+          <Link
             className="hover:underline text-sm"
             href={`/my-route/${index + 1}`}
           >
             Route {index + 1}
-          </HowerPrefetchLink>
+          </Link>
         </li>
       ))}
     </ul>
