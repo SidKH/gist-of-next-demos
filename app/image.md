755ada62094520198b79fec3a103b57966fa22d9
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-07-19 19:57:55 +0100

    demo

---
+++ app/image.md
@@ -0,0 +1,3 @@
+Image from a different cloud 👇
+
+https://res.cloudinary.com/dnyph738o/image/upload/v1752949459/cld-sample-5.jpg
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   images: {
     remotePatterns: [
       new URL("https://res.cloudinary.com/**"),
