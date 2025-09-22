4caa7e303be9ac43c7b57b65ab716e2f07335f52
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-09-22 15:46:51 +0100

    Empty template

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <div
-        style={{ height: "100svh" }}
-        className="relative flex gap-4 items-center justify-center"
-      >
-        <img
-          src="https://picsum.photos/id/846/1000/1000"
-          className="absolute w-full h-full object-cover"
-        />
-        <div className="absolute bg-black/70 text-white left-0 top-0 w-full h-full flex items-center justify-center text-5xl font-bold">
-          100svh
-        </div>
-        <p className="absolute bottom-0 text-center p-4 left-0 w-full">
-          Scroll to continue
-        </p>
+      <div className="flex items-center justify-center h-screen">
+        <h1>Hello World</h1>
       </div>
-      <div className="relative h-screen bg-white text-black flex items-center justify-center text-4xl font-bold" />
     </div>
   );
 }
