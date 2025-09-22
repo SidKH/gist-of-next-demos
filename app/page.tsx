8cf23507f6f4d5dc47c9d8bb0947b8bad737b273
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-09-22 15:45:57 +0100

    Deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div className="flex gap-4 items-center h-screen justify-center">
-      Home
+    <div>
+      <div
+        style={{ height: "100svh" }}
+        className="relative flex gap-4 items-center justify-center"
+      >
+        <img
+          src="https://picsum.photos/id/846/1000/1000"
+          className="absolute w-full h-full object-cover"
+        />
+        <div className="absolute bg-black/70 text-white left-0 top-0 w-full h-full flex items-center justify-center text-5xl font-bold">
+          100svh
+        </div>
+        <p className="absolute bottom-0 text-center p-4 left-0 w-full">
+          Scroll to continue
+        </p>
+      </div>
+      <div className="relative h-screen bg-white text-black flex items-center justify-center text-4xl font-bold" />
     </div>
   );
 }
