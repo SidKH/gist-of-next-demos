e55f7d030fba319ad62e34cb635080c880340a5e
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-09-28 13:34:00 +0100

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
+import { cookies } from "next/headers";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get("myCookie");
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
-        <h1>Hello World</h1>
+      <div className="flex flex-col gap-6 items-center justify-center h-screen">
+        <p className="text-7xl font-medium">
+          {cookie?.value}
+        </p>
+        <form
+          action={async function () {
+            "use server";
+            const cookieStore = await cookies();
+            const cookie = cookieStore.get("myCookie");
+            cookieStore.set(
+              "myCookie",
+              (
+                Number(cookie?.value ?? 0) + 1
+              ).toString()
+            );
+          }}
+        >
+          <Button>Increment cookie value</Button>
+        </form>
       </div>
     </div>
   );
