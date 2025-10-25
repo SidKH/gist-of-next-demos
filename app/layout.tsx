4297ed2d44fa8d8bd4a0ef44f7a3e35f99b873e8
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-10-25 20:09:40 +0100

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
