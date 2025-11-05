69086602bdf1c1aec5ab1d3ba65e844325a9ea91
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-11-05 18:50:12 +0000

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { refresh, revalidatePath } from "next/cache";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center flex-col gap-4 justify-center bg-zinc-50 font-sans dark:bg-black">
-      <CachedComponent />
-      <Suspense>
-        <DynamicComponent />
-      </Suspense>
       <form
         action={async () => {
           "use server";
@@ -25,27 +20,6 @@ export default function Home() {
       >
         <Button>refresh</Button>
       </form>
-    </div>
-  );
-}
-
-async function CachedComponent() {
-  "use cache";
-  const response = await fetch("https://randomuser.me/api/");
-  const data = await response.json();
-  return (
-    <div className="border-dashed border-2 p-4 w-40 text-center rounded-md">
-      {data.results[0].name.first}
-    </div>
-  );
-}
-
-async function DynamicComponent() {
-  const response = await fetch("https://randomuser.me/api/");
-  const data = await response.json();
-  return (
-    <div className="border-dashed border-2 p-4 w-40 text-center rounded-md">
-      {data.results[0].name.first}
     </div>
   );
 }
