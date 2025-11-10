ce557f6248532ce6455dcddcde471bf43ef1f70c
Alex Sidorenko <my.mail.kharkov@gmail.com>
2025-11-10 15:32:00 +0000

    demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,6 @@ export default function RootLayout({
               <li>
                 <NavLink href="/dynamic">Dynamic Page</NavLink>
               </li>
-              <li>
-                <NavLink href="/dynamic-route/1">Dynamic Route</NavLink>
-              </li>
             </ul>
           </nav>
         </aside>
